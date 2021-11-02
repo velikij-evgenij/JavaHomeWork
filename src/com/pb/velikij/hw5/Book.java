3eee2ce0dc88b41610f6da6140cfa0989a6d9c67
Evgenij Velikij <velikiyep@gmail.com>
2021-11-03 00:53:45 +0300

    init commit

--- src/com/pb/velikij/hw5/Book.java
+++ src/com/pb/velikij/hw5/Book.java
@@ -2,9 +2,36 @@ package com.pb.velikij.hw5;
 
 public class Book {
 
-    String bookName;
-    String bookAvtor;
-    int bookYear;
+    private String bookName;
+    private String bookAvtor;
+    private int bookYear;
+
+    public String getBookName() {
+        if (bookName == null || bookName.isEmpty()) {
+            System.out.println("Название книги необходимо указать!");
+        }
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getBookAvtor() {
+        return bookAvtor;
+    }
+
+    public void setBookAvtor(String bookAvtor) {
+        this.bookAvtor = bookAvtor;
+    }
+
+    public int getBookYear() {
+        return bookYear;
+    }
+
+    public void setBookYear(int bookYear) {
+        this.bookYear = bookYear;
+    }
 
     String bookInfo() {
         return "Книга: " + bookName + ", автор: " + bookAvtor + ", год издания: " + bookYear;
--- src/com/pb/velikij/hw5/Reader.java
+++ src/com/pb/velikij/hw5/Reader.java
@@ -2,11 +2,59 @@ package com.pb.velikij.hw5;
 
 public class Reader {
 
-    String readerFamily;
-    int readerTicket;
-    String readerFaculty;
-    String readerBirthday;
-    String readerPhone;
+    private String readerFamily;
+    private int readerTicket;
+    private String readerFaculty;
+    private String readerBirthday;
+    private String readerPhone;
+
+    public String getReaderFamily() {
+        if (readerFamily == null || readerFamily.isEmpty()) {
+            System.out.println("Фамилию читателя необходимо указать!");
+        }
+        return readerFamily;
+    }
+
+    public void setReaderFamily(String readerFamily) {
+        this.readerFamily = readerFamily;
+    }
+
+    public int getReaderTicket() {
+        return readerTicket;
+    }
+
+    public void setReaderTicket(int readerTicket) {
+        this.readerTicket = readerTicket;
+    }
+
+    public String getReaderFaculty() {
+        return readerFaculty;
+    }
+
+    public void setReaderFaculty(String readerFaculty) {
+        this.readerFaculty = readerFaculty;
+    }
+
+    public String getReaderBirthday() {
+        return readerBirthday;
+    }
+
+    public void setReaderBirthday(String readerBirthday) {
+        this.readerBirthday = readerBirthday;
+    }
+
+    public String getReaderPhone() {
+        return readerPhone;
+    }
+
+    public void setReaderPhone(String readerPhone) {
+        this.readerPhone = readerPhone;
+    }
+
+    int n = 3;
+    public void takeBook(String readerFamily) {
+        System.out.println(readerFamily + " взял " + n +" книги.");
+    }
 
     String readerInfo() {
         return "Читатель: " + readerFamily + ", номер билета: " + readerTicket + ", факультет: " + readerFaculty + ", дата рождения: " + readerBirthday + ", телефон: " + readerPhone;
