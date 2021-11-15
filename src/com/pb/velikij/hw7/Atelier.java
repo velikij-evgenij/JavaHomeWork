80f25d7131b727db86ffcc6348b401c4889d637c
Evgenij Velikij <velikiyep@gmail.com>
2021-11-15 03:32:25 +0300

    init commit

--- src/com/pb/velikij/hw7/Atelier.java
+++ src/com/pb/velikij/hw7/Atelier.java
@@ -1,26 +1,26 @@
 package com.pb.velikij.hw7;
 
+import com.pb.velikij.hw6.Animal;
+
 import java.util.Arrays;
 
 public class Atelier {
 
     public static void main(String[] args) {
 
-
-        Size.L.getEuroSize("LO");
-
         Clothes.Tshirt Tshirt1 = new Clothes.Tshirt(Size.XXS, 125, "белый");
-        Tshirt1.dressMan();
+        Clothes.Pants Pants1 = new Clothes.Pants(Size.S, 455, "черный");
+        Clothes.Skirt Skirt1 = new Clothes.Skirt(Size.L, 345, "синий");
+        Clothes.Tie Tie1 = new Clothes.Tie(Size.M, 22,"зеленый");
 
-        Clothes.Tshirt Tshirt2 = new Clothes.Tshirt(Size.S, 455, "черный");
-        Tshirt2.dressWomen();
+        Clothes[] clothesArray = new Clothes[] {Tshirt1, Pants1, Skirt1, Tie1};
 
-        Clothes.Tie Tie1 = new Clothes.Tie(Size.M, 22,"dsa");
-        Tie1.dressWomen();
+        public void dressMan(Clothes[] clothesArray) {
 
-        Clothes.Skirt Skirt1 = new Clothes.Skirt(Size.L, 345, "апмва");
-        Skirt1.dressMan();
-
+            Object obj = clothesArray;
+            if (obj instanceof dressMan) {
+                obj.dressMan;
+            }
         }
-
     }
+}
