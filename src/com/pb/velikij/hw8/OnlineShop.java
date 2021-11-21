26e1733898cb70cc0bb5c9b08b2c8a66a6a9191c
Evgenij Velikij <velikiyep@gmail.com>
2021-11-21 22:41:56 +0300

    init commit

--- src/com/pb/velikij/hw8/OnlineShop.java
+++ src/com/pb/velikij/hw8/OnlineShop.java
@@ -1,17 +1,26 @@
 package com.pb.velikij.hw8;
 
-public class OnlineShop {
+public class OnlineShop {                                                                  //создание класса "Онлайн магазин"
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {                                               //создание метода "главный"
 
-        Auth Auth1 = new Auth();
+        Auth User1 = new Auth();                                                           //создание объекта "Пользователь1"
 
-        for (int i=5; i>0;) {
-            try {
-                Auth1.signUp();
-                i=0;
-            } catch (Auth.WrongLoginException | Auth.WrongPasswordException e1) {
-                System.out.println("Для регистрации сталось попыток: " + --i);
+        for (int i=5; i>0;) {                                                              //создание цикла подсчета ошибок
+            try {                                                                          //создание обработки исключений
+                User1.signUp();                                                            //вызов метода регистрации
+                i=0;                                                                       //обнуление счетчика для завершения цикла
+            } catch (Auth.WrongLoginException | Auth.WrongPasswordException exSignUp) {    //проверка на определенное исключение
+                System.out.println("Для регистрации осталось попыток: " + --i + "\n");     //уменьшение счетчика ошибок
+            }
+        }
+
+        for (int i=5; i>0;) {                                                              //создание цикла подсчета ошибок
+            try {                                                                          //создание обработки исключений
+                User1.signIn();                                                            //вызов метода регистрации
+                i=0;                                                                       //обнуление счетчика для завершения цикла
+            } catch (Auth.WrongLoginException exSignIn) {                                  //проверка на определенное исключение
+                System.out.println("Для входа на сайт осталось попыток: " + --i + "\n");   //уменьшение счетчика ошибок
             }
         }
     }
